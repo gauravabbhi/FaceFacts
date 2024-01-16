21129a0414efae2a9a5d3a013595d4281bbe2357
Gaurav Abbhi <gaurav.abbh@me.com>
2024-01-16 16:50:32 -0500

    Initial Commit

---
+++ FaceFacts/FaceFactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceFactsApp.swift
+//  FaceFacts
+//
+//  Created by Karan on 16/01/24.
+//
+
+import SwiftUI
+
+@main
+struct FaceFactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
